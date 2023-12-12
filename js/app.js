3829ff33ac6558efe9f25c36e08a68a6cc8f0e57
Xavier Kooijman <xavierkooijman@gmail.com>
2023-12-12 23:52:59 +0000

    Animated Modal and Rainbow border for header

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 modal = document.getElementById('modalBackground')
 addBookButton = document.getElementById('addBookButton')
+bookTitleInput = document.getElementById('bookTitle')
+bookAuthorInput = document.getElementById('bookAuthor')
+bookPagesInput = document.getElementById('bookPages')
+bookStatusInput = document.getElementById('bookStatus')
+
+bookColors = ['#f79533', '#f37055', '#ef4e7b', '#a166ab', '#5073b8', '#1098ad', '#07b39b', '#6fba82', '#ff6f4f', '#dac6fb', '#029c54']
 
 addBookButton.addEventListener('click', () => {
   modal.style.display = "flex"
