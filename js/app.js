e58fbea8159068f3a203136cf3b02ab217f40099
Xavier Kooijman <xavierkooijman@gmail.com>
2023-12-13 00:39:46 +0000

    Constructor for adding books, function for adding books to an array

--- js/app.js
+++ js/app.js
@@ -4,8 +4,27 @@ bookTitleInput = document.getElementById('bookTitle')
 bookAuthorInput = document.getElementById('bookAuthor')
 bookPagesInput = document.getElementById('bookPages')
 bookStatusInput = document.getElementById('bookStatus')
+addBookForm = document.getElementById('addBookForm')
 
 bookColors = ['#f79533', '#f37055', '#ef4e7b', '#a166ab', '#5073b8', '#1098ad', '#07b39b', '#6fba82', '#ff6f4f', '#dac6fb', '#029c54']
+let bookLibrary = []
+
+function Book(title, author, pages, read){
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+function addBookToLibrary(){
+  let title = bookTitleInput.value
+  let author = bookAuthorInput.value
+  let pages = bookPagesInput.value
+  let read = bookStatusInput.checked
+  const book = new Book(title,author,pages,read)
+  bookLibrary.push(book)
+  console.log(bookLibrary)
+}
 
 addBookButton.addEventListener('click', () => {
   modal.style.display = "flex"
@@ -16,3 +35,8 @@ window.onclick = function(event) {
     modal.style.display = "none"
   }
 }
+
+addBookForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  addBookToLibrary()
+})
