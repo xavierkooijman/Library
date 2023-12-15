57891e1a42e9ee06bd6fc99597b83913d796f7a2
Xavier Kooijman <xavierkooijman@gmail.com>
2023-12-15 18:33:54 +0000

    functions to add new books and display them

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ bookAuthorInput = document.getElementById('bookAuthor')
 bookPagesInput = document.getElementById('bookPages')
 bookStatusInput = document.getElementById('bookStatus')
 addBookForm = document.getElementById('addBookForm')
+booksContainer = document.getElementById('booksContainer')
 
 bookColors = ['#f79533', '#f37055', '#ef4e7b', '#a166ab', '#5073b8', '#1098ad', '#07b39b', '#6fba82', '#ff6f4f', '#dac6fb', '#029c54']
 let bookLibrary = []
@@ -23,7 +24,21 @@ function addBookToLibrary(){
   let read = bookStatusInput.checked
   const book = new Book(title,author,pages,read)
   bookLibrary.push(book)
-  console.log(bookLibrary)
+  return book
+}
+
+function displayNewBook(book){
+  modal.style.display = "none"
+  const newDiv = document.createElement('div')
+  newDiv.classList.add("book")
+  const title = document.createElement('p')
+  title.innerText = `"${book.title}"`
+  const author = document.createElement('p')
+  author.innerText = book.author
+  const pages = document.createElement('p')
+  pages.innerText = `${book.pages} pages`
+  newDiv.append(title,author,pages)
+  booksContainer.appendChild(newDiv)
 }
 
 addBookButton.addEventListener('click', () => {
@@ -38,5 +53,6 @@ window.onclick = function(event) {
 
 addBookForm.addEventListener('submit', (event) => {
   event.preventDefault()
-  addBookToLibrary()
+  let book = addBookToLibrary()
+  displayNewBook(book)
 })
